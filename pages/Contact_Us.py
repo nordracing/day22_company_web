ef836dfee836a553be265dd99edc990c7f5bef7f
igorl <igor.litviennco@gmail.com>
2024-11-27 20:46:47 +0200

    final config, Day 23

---
+++ pages/Contact_Us.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from send_email import send_email
+import pandas
+
+df = pandas.read_csv("topics.csv")
+
+with st.form(key="email_form"):
+    user_email = st.text_input("Your email address")
+    option1 = st.selectbox(
+        "What topic do you want to discuss?",
+        df["topic"])
+    raw_message = st.text_area("Text")
+    message = f"""\
+Subject: New email from {user_email}
+
+From: {user_email}
+Topic: {option1}
+{raw_message}
+"""
+    button = st.form_submit_button("Submit")
+    if button:
+        send_email(message)
+        st.info("Your message was sent successfully")
